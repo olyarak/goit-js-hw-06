6e39a0f91c04f95354629d85d7eeac0d446180ef
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-01-21 23:46:51 +0200

    Add first task

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+const categoriesEl = document.querySelector("#categories");
+console.log(categoriesEl);
