0668bc342dbd792ffbea434463bdcfc4801137c0
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-01-22 01:16:41 +0200

    tasks 2 and 3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,8 @@
-// Напиши скрипт, який:
+const categoriesItemsEl = document.querySelectorAll("li.item");
 
-// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+console.log(`Number of categories: ${categoriesItemsEl.length}`);
 
-const categoriesEl = document.querySelector("#categories");
-console.log(categoriesEl);
+categoriesItemsEl.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+function onCreateItem(array) {
+  return array.map((item) => {
+    const arrayItem = document.createElement("li");
+    arrayItem.textContent = item;
+    arrayItem.classList.add("item");
+    return arrayItem;
+  });
+}
+
+ingredientsEl.append(...onCreateItem(ingredients));
