727ac5870121737f2728ced97d5ee6a0ef649f10
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-01-22 23:37:11 +0200

    Task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  span.textContent = color;
+}
