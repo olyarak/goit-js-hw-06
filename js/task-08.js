5915103aa4e0b615f75ac4eea245254e2877e2a0
Olha Rakytianska <olya.rakytyanska@gmail.com>
2023-01-23 11:01:55 +0200

    Update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -10,13 +10,13 @@ function onSubmit(event) {
   } = event.currentTarget;
 
   if (email.value === "" || password.value === "") {
-    alert("Усі поля повинні бути заповнені");
+    return alert("Усі поля повинні бути заповнені");
   } else {
     const formData = {
       email: email.value,
       password: password.value,
     };
     console.log(formData);
+    event.currentTarget.reset();
   }
-  event.currentTarget.reset();
 }
